76e8c67f8591b0ae6e7731620530a86327fdd345
Vladosik <vladdrozdov7875@gmail.com>
2025-03-06 14:20:35 +0200

    save-2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Main {
+
+    static String operator = "";
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
         f.setSize(300,400);
         f.setLayout(new BorderLayout());
-        JTextField display = new JTextField("0");
+        JTextField display = new JTextField();
         display.setHorizontalAlignment(JTextField.RIGHT);
         display.setFont(new Font( "",Font.BOLD, 30));
         f.add(display, BorderLayout.NORTH);
@@ -20,11 +25,66 @@ public class Main {
                 "1", "2", "3", "-",
                 "C", "0", "=", "+"
         };
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
         for (int i = 0; i < 16; i++) {
             JButton b = new JButton(buttonsName[i]);
             panel.add(b);
+            b.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String bText = b.getText();
+                    String displayText = display.getText();
+                    switch (bText){
+                        case "1","2","3","4","5","6","7","8","9","0":
+                            display.setText(displayText+bText);
+                            break;
+                        case "/","*","-","+":
+                            operator = bText;
+                            numbers.add(Integer.parseInt(displayText));
+                            display.setText(operator);
+//                        System.out.println(numbers.get(0));
+                            break;
+                        case "=":
+                            if (numbers.size()>=1){
+                                numbers.add(Integer.parseInt(displayText));
+                                int result = 0;
+                                switch (operator){
+                                    case"+":
+                                        result=numbers.get(0) + numbers.get(1);
+                                        break;
+                                    case"-":
+                                        result=numbers.get(0) - numbers.get(1);
+                                        break;
+                                    case"/":
+                                        result=numbers.get(0) / numbers.get(1);
+                                        break;
+                                    case"*":
+                                        result=numbers.get(0) * numbers.get(1);
+                                        break;
+                                }
+                                numbers.clear();
+                                numbers.add(result);
+                                display.setText(String.valueOf(result));
+                            }
+
+                            break;
+                        case "C":
+                            display.setText("");
+                            numbers.clear();
+                            break;
+                    }
+
+                }
+            });
         }
+
+
+
+
         f.add(panel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
 
 
